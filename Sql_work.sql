4ac048dfc39a25aa7b4207367555e2a0d604fa9c
Akki283 <akshaygaikwad283@gmail.com>
2023-04-12 17:43:12 +0530

    second

--- Sql_work.sql
+++ Sql_work.sql
@@ -29,5 +29,62 @@ update cust
 set salary='5000';
 
 update cust
-set ename='max' , salary=5000
+set ename='max' , salary=7000
 where id=15;
+ 
+-- update salary with null values
+
+update cust
+set salary =''
+where ename='max';
+
+update cust 
+set salary = 16000
+where salary is null;
+
+delete from cust where salary ='16000';
+
+describe cust;
+alter table cust add (bonus number (10));
+
+select (salary+bonus) from cust;
+
+select (salary+bonus) total  from cust;
+
+select cust.*,(salary-bonus) total  from cust;
+
+select cust.*,(salary*bonus) total  from cust;
+
+select cust.*,(salary/bonus) total  from cust where gender = 'male';
+
+select *from cust where bonus>50;
+
+select *from cust where bonus<=50;
+
+select *from cust where bonus!=50;
+
+--between
+
+select * from cust where bonus between 50 and 100;
+
+--not between
+
+select * from cust where bonus not  between 60 and 100;
+
+--in
+
+select * from cust where salary in 5000;
+
+select * from cust where ename in ('amit','sujit');
+
+--not in
+
+select * from cust where ename not in ('amit','sujit');
+
+--concatination ||
+
+
+select ename||'__hehe__'||prof from cust;
+
+
+;
